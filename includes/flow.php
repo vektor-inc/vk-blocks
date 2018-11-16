73374b8d200bfb792e60c62cb3736cd7d81f0596
kurudrive <kurudrive@gmail.com>
2018-11-16 11:50:32 +0900

    [ flow ] editor style 設定

--- includes/flow.php
+++ includes/flow.php
@@ -11,6 +11,7 @@ add_action(
 	'init', function() {
 		// Register CSS
 		wp_register_style( 'vk-block-flow', VK_BLOCKS_ASSET_URL . 'assets/css/flow.css', [], VK_BLOCKS_VERSION );
+		wp_register_style( 'vk-block-flow-editor', VK_BLOCKS_ASSET_URL . 'assets/css/flow-editor.css', [], VK_BLOCKS_VERSION );
 		// Register JS
 		wp_register_script( 'vk-block-flow', VK_BLOCKS_ASSET_URL . 'assets/js/flow.js', [ 'wp-element', 'wp-blocks' ], VK_BLOCKS_VERSION, true );
 		// Register block.
@@ -18,7 +19,7 @@ add_action(
 			// Alert Block.
 			register_block_type(
 				'vk-block/flow', [
-					'editor_style'  => 'vk-block-flow',
+					'editor_style'  => 'vk-block-flow-editor',
 					'editor_script' => 'vk-block-flow',
 				]
 			);
