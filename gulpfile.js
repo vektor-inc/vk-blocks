13ba9f7fe7468679c1a3ed88383001d38ecb4bae
kurudrive <kurudrive@gmail.com>
2018-11-22 17:33:58 +0900

    [ editor.css ] 調整

--- gulpfile.js
+++ gulpfile.js
@@ -27,38 +27,16 @@ gulp.task('sass', function () {
         //bundle css files by gulp-concat
         .pipe(concat('block-build.css'))
         .pipe(gulp.dest('./dist'));
-
-		// gulp.src(['./src/editor.scss'])
-    //     .pipe($.plumber({
-    //         errorHandler: $.notify.onError('<%= error.message %>')
-    //     }))
-    //     .pipe($.sourcemaps.init({loadMaps: true}))
-    //     .pipe($.sass({
-    //         errLogToConsole: true,
-    //         outputStyle: 'compressed',
-    //         includePaths: [
-    //             './src/scss'
-    //         ]
-    //     }))
-    //     .pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
-    //     .pipe($.sourcemaps.write('./map'))
-		//
-    //     //bundle css files by gulp-concat
-    //     .pipe(concat('block-build-editor.css'))
-    //     .pipe(gulp.dest('./dist'));
 });
 
 
 gulp.task('sass_editor', function () {
-    return gulp.src([ './src/editor.scss_before', './src/**/*.scss', './src/editor.scss_after'])
-							.pipe(cleanCss())
-						  .pipe(concat('editor-block-build.scss'))
-							.pipe(gulp.dest('./src'))
-							.pipe(sass())
-							.pipe(concat('block-build-editor.css'))
-							.pipe(autoprefixer())
-							.pipe(cleanCss())
-						  .pipe(gulp.dest('./dist'));
+    return gulp.src([ './editor-css/editor.scss_before',  './src/**/*.scss', './editor-css/editor.scss_after'])
+				.pipe(concat('editor-block-build.scss'))
+				.pipe(gulp.dest('./editor-css/'))
+				.pipe(sass())
+				.pipe(concat('block-build-editor.css'))
+				.pipe(gulp.dest('./dist/'));
 });
 
 
@@ -73,6 +51,7 @@ gulp.task('js', function () {
 gulp.task('watch', function () {
     gulp.watch('src/**/*.js', ['js']);
     gulp.watch('src/**/*.scss', ['sass','sass_editor']);
+    // gulp.watch('src/**/*.scss', ['sass']);
 });
 
 // Build
