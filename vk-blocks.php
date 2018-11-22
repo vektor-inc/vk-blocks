c3a685910297e51a0af9c095c87f1aae2f8c583f
kurudrive <kurudrive@gmail.com>
2018-11-22 15:33:15 +0900

    phpcs

--- vk-blocks.php
+++ vk-blocks.php
@@ -18,26 +18,28 @@ define( 'VK_BLOCKS_VERSION', '1.0.0' );
 // Set asset URL.
 define( 'VK_BLOCKS_URL', plugin_dir_url( __FILE__ ) );
 
-add_action( 'plugins_loaded', function () {
-	//Load language files.
-	load_plugin_textdomain( 'vk-blocks', false, basename( dirname( __FILE__ ) ) . '/dist/languages' );
-} );
+add_action(
+	'plugins_loaded', function () {
+		//Load language files.
+		load_plugin_textdomain( 'vk-blocks', false, basename( dirname( __FILE__ ) ) . '/dist/languages' );
+	}
+);
 
 function vkblocks_blocks_assets() {
 
 	wp_register_style( 'vk-blocks-buid-css', plugins_url( 'dist/block-build.css', __FILE__ ), [], VK_BLOCKS_VERSION );
 	wp_register_script(
 		'vk-blocks-buid-js', plugins_url( 'dist/block-build.js', __FILE__ ), array(
-		'wp-blocks',
-		'wp-i18n',
-		'wp-element',
-	), VK_BLOCKS_VERSION, true
+			'wp-blocks',
+			'wp-i18n',
+			'wp-element',
+		), VK_BLOCKS_VERSION, true
 	);
 
 	if ( function_exists( 'gutenberg_get_jed_locale_data' ) ) {
 
 		//Get translation data from indicated text-domain.
-		$locale  = gutenberg_get_jed_locale_data( 'vk-blocks' );
+		$locale = gutenberg_get_jed_locale_data( 'vk-blocks' );
 
 		//Add translation data to javasript object.
 		$content = 'wp.i18n.setLocaleData( ' . json_encode( $locale ) . ', "vk-blocks" );';
@@ -51,28 +53,28 @@ function vkblocks_blocks_assets() {
 		// Alert Block.
 		register_block_type(
 			'vk-blocks/alert', [
-				'style'  => 'vk-blocks-buid-css',
+				'style'         => 'vk-blocks-buid-css',
 				'editor_script' => 'vk-blocks-buid-js',
 			]
 		);
 		// Baloon Block.
 		register_block_type(
 			'vk-blocks/balloon', [
-				'style'  => 'vk-blocks-buid-css',
+				'style'         => 'vk-blocks-buid-css',
 				'editor_script' => 'vk-blocks-buid-js',
 			]
 		);
 		// Faq Block.
 		register_block_type(
 			'vk-blocks/faq', [
-				'style'  => 'vk-blocks-buid-css',
+				'style'         => 'vk-blocks-buid-css',
 				'editor_script' => 'vk-blocks-buid-js',
 			]
 		);
 		// Flow Block.
 		register_block_type(
 			'vk-blocks/flow', [
-				'style'  => 'vk-blocks-buid-css',
+				'style'         => 'vk-blocks-buid-css',
 				'editor_script' => 'vk-blocks-buid-js',
 			]
 		);
@@ -87,7 +89,7 @@ function vkblocks_blocks_categories( $categories, $post ) {
 		$categories,
 		array(
 			array(
-				'slug' => 'vk-blocks-cat',
+				'slug'  => 'vk-blocks-cat',
 				'title' => __( 'Vektor Blocks（Beta）', 'vk-blocks' ),
 				'icon'  => '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z" /><path d="M19 13H5v-2h14v2z" /></svg>',
 			),
