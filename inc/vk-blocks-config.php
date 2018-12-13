7fc852efc8716034fa2a12640b73506b62592016
naoki <n.globe.us@gmail.com>
2018-12-13 18:04:41 +0900

    管理画面の投稿ページのみBootstrapを適用

--- inc/vk-blocks-config.php
+++ inc/vk-blocks-config.php
@@ -13,9 +13,19 @@ if ( ! function_exists( 'vkblocks_active' ) ) {
 	global $vk_blocks_prefix;
 	$vk_blocks_prefix = apply_filters( 'vk_blocks_prefix', 'VK ' );
 
-	function vkblocks_load_bootstrap_admin() {
-		wp_register_style( 'vkblocks-bootstrap', plugin_dir_url(__FILE__) . 'vk-blocks/plugins/bootstrap-4.1.3/css/bootstrap.min.css', false, '4.1.3' );
-		wp_enqueue_style( 'vkblocks-bootstrap' );
+	function vkblocks_load_bootstrap_admin( $hook_suffix ) {
+
+		if ( is_admin() ) {
+
+			if ( 'post.php' === $hook_suffix || 'post-new.php' === $hook_suffix ) {
+				wp_register_style( 'vkblocks-bootstrap', plugin_dir_url( __FILE__ ) . 'vk-blocks/plugins/bootstrap-4.1.3/css/bootstrap.min.css', false, '4.1.3' );
+				wp_enqueue_style( 'vkblocks-bootstrap' );
+			}
+		} else {
+			wp_register_style( 'vkblocks-bootstrap', plugin_dir_url( __FILE__ ) . 'vk-blocks/plugins/bootstrap-4.1.3/css/bootstrap.min.css', false, '4.1.3' );
+			wp_enqueue_style( 'vkblocks-bootstrap' );
+		}
+
 	}
 	add_action( 'admin_enqueue_scripts', 'vkblocks_load_bootstrap_admin' );
 	add_action( 'wp_enqueue_scripts', 'vkblocks_load_bootstrap_admin' );
