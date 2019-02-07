a96cc6ba0c75ceb414e7d29ffc742062241ab92e
kurudrive <kurudrive@gmail.com>
2019-02-07 14:38:48 +0900

    [ Change version ] 0.4.2

--- vk-blocks.php
+++ vk-blocks.php
@@ -3,7 +3,7 @@
  * Plugin Name: VK Blocks
  * Plugin URI: https://github.com/vektor-inc/vk-blocks
  * Description: This is a plugin that extends Gutenberg's blocks.
- * Version: 0.4.1
+ * Version: 0.4.2
  * Author: Vektor,Inc.
  * Author URI:
  * Text Domain: vk-blocks
