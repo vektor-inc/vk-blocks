da01aa9660bd75ae1a9a9b2d7cc142fa9135820e
kurudrive <kurudrive@gmail.com>
2019-02-12 02:03:45 +0900

    [ heading ] CSS調整

--- src/heading/block.js
+++ src/heading/block.js
@@ -3,7 +3,11 @@ wp.domReady( function() {
     wp.blocks.registerBlockStyle('core/heading',
         [
             {name: 'vk_heading-regular', label: __('Regular', 'vk-blocks'), isDefault: true},
-            {name: 'vk_heading-stripes', label: __('Stripes', 'vk-blocks')},
+            {name: 'vk_heading-style-plain', label: __('Plain', 'vk-blocks')},
+            {name: 'vk_heading-style-plainNoMargin', label: __('Plain (No margin)', 'vk-blocks')},
+            {name: 'vk_heading-style-borderBottomSolidNormal', label: __('Border bottom', 'vk-blocks')},
+						{name: 'vk_heading-style-borderBottomSolidHeavy', label: __('Border bottom heavy', 'vk-blocks')},
+
         ],
     );
 } );
