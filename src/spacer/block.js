3e12938736f2625d13bf19f0c8a774c25507d580
kurudrive <kurudrive@gmail.com>
2019-02-21 02:37:16 +0900

    [ spacer ] SVGアイコン作成

--- src/spacer/block.js
+++ src/spacer/block.js
@@ -11,7 +11,16 @@ const {registerBlockType} = wp.blocks; // Import registerBlockType() from wp.blo
 const {RangeControl, PanelBody, BaseControl,SelectControl} = wp.components;
 const {Fragment} = wp.element;
 const {InspectorControls} = wp.editor;
-const BlockIcon = 'arrow-down';
+const BlockIcon = (
+	<svg xmlns="http://www.w3.org/2000/svg" width="576" height="512" viewBox="0 0 576 512">
+	<g>
+		<rect x="108.8" y="18.7" width="358.5" height="40"/>
+		<rect x="108.8" y="453.3" width="358.5" height="40"/>
+		<polygon points="171.4,253.2 131.4,253.2 131.4,412.6 290.8,412.6 290.8,372.6 199.7,372.6 404.6,167.7 404.6,258.8 444.6,258.8
+			444.6,99.4 285.2,99.4 285.2,139.4 376.3,139.4 171.4,344.3 	"/>
+	</g>
+	</svg>
+);
 
 /**
  * Register: a Gutenberg Block.
@@ -28,9 +37,9 @@ const BlockIcon = 'arrow-down';
  */
 registerBlockType('vk-blocks/spacer', {
     // Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-    title: __('Spacer', 'vk-blocks'), // Block title.
+    title: __('Responsive Spacer', 'vk-blocks'), // Block title.
     icon: BlockIcon, // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
-    category: 'vk-blocks-cat', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+    category: 'vk-blocks-cat-layout', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
     attributes: schema,
 
     /**
