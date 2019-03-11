a8e5a36ec496e7155aa3214a1af1a661cbd1fcd8
naoki <n.globe.us@gmail.com>
2019-03-11 12:33:55 +0900

    headingのカスタムクラスがundefiendになる問題を修正

--- src/heading/block.js
+++ src/heading/block.js
@@ -3,6 +3,7 @@
  *
  */
 import React from "react";
+import classNames from 'classnames';
 import {schema} from './schema';
 import HeadingToolbar from './heading-toolbar';
 import {Version0_6_0} from './deprecated/block';
@@ -108,6 +109,9 @@ registerBlockType('vk-blocks/heading', {
             }
         };
 
+        let containerClass = classNames(className,`vk_heading vk_heading-style-${titleStyle}`);
+
+
         return (
             <Fragment>
                 <BlockControls>
@@ -198,7 +202,7 @@ registerBlockType('vk-blocks/heading', {
                 </InspectorControls>
                 {
                     outerMarginBottom == null ?
-                        <div className={`${className} vk_heading vk_heading-style-${titleStyle}`}>
+                        <div className={containerClass}>
                             <RichText
                                 tagName={tagName}
                                 value={title}
@@ -235,7 +239,7 @@ registerBlockType('vk-blocks/heading', {
                         </div>
                         :
                         <div
-                            className={`${className} vk_heading vk_heading-style-${titleStyle}`}
+                            className={containerClass}
                             style={{marginBottom: outerMarginBottom + `rem`}}
                         >
                             <RichText
@@ -290,80 +294,77 @@ registerBlockType('vk-blocks/heading', {
         const {level, align, title, titleColor, titleSize, subText, subTextFlag, subTextColor, subTextSize, titleStyle, titleMarginBottom, outerMarginBottom} = attributes;
         const tagName = 'h' + level;
 
-        return (
-            <Fragment>
+        let containerClass = classNames(className,`vk_heading vk_heading-style-${titleStyle}`);
+
+        if(outerMarginBottom == null){
+            return(<div className={containerClass}>
+                <RichText.Content
+                    tagName={tagName}
+                    value={title}
+                    style={{
+                        color: titleColor,
+                        fontSize: titleSize + 'rem',
+                        textAlign: align,
+                    }}
+                    className={`vk_heading_title vk_heading_title-style-${titleStyle}`}
+                />
                 {
-                    outerMarginBottom == null ?
-                        <div className={`${className} vk_heading vk_heading-style-${titleStyle}`}>
-                            <RichText.Content
-                                tagName={tagName}
-                                value={title}
-                                style={{
-                                    color: titleColor,
-                                    fontSize: titleSize + 'rem',
-                                    textAlign: align,
-                                }}
-                                className={`vk_heading_title vk_heading_title-style-${titleStyle}`}
-                            />
-                            {
-                                // サブテキスト
-                                (() => {
-                                    if (subTextFlag === 'on') {
-                                        return (
-                                            <RichText.Content
-                                                tagName={'p'}
-                                                value={subText}
-                                                style={{
-                                                    color: subTextColor,
-                                                    fontSize: subTextSize + 'rem',
-                                                    textAlign: align
-                                                }}
-                                                className={`vk_heading_subtext vk_heading_subtext-style-${titleStyle}`}
-                                            />
-                                        );
-                                    }
-                                })()
-                            }
-                        </div>
-                        :
-                        <div
-                            className={`${className} vk_heading vk_heading-style-${titleStyle}`}
-                            style={{marginBottom: outerMarginBottom + `rem`}}
-                        >
-                            <RichText.Content
-                                tagName={tagName}
-                                value={title}
-                                style={{
-                                    color: titleColor,
-                                    fontSize: titleSize + 'rem',
-                                    textAlign: align,
-                                    marginBottom: titleMarginBottom + 'rem'
-                                }}
-                                className={`vk_heading_title vk_heading_title-style-${titleStyle}`}
-                            />
-                            {
-                                // サブテキスト
-                                (() => {
-                                    if (subTextFlag === 'on') {
-                                        return (
-                                            <RichText.Content
-                                                tagName={'p'}
-                                                value={subText}
-                                                style={{
-                                                    color: subTextColor,
-                                                    fontSize: subTextSize + 'rem',
-                                                    textAlign: align
-                                                }}
-                                                className={`vk_heading_subtext vk_heading_subtext-style-${titleStyle}`}
-                                            />
-                                        );
-                                    }
-                                })()
-                            }
-                        </div>
+                    // サブテキスト
+                    (() => {
+                        if (subTextFlag === 'on') {
+                            return (
+                                <RichText.Content
+                                    tagName={'p'}
+                                    value={subText}
+                                    style={{
+                                        color: subTextColor,
+                                        fontSize: subTextSize + 'rem',
+                                        textAlign: align
+                                    }}
+                                    className={`vk_heading_subtext vk_heading_subtext-style-${titleStyle}`}
+                                />
+                            );
+                        }
+                    })()
                 }
-            </Fragment>
-        );
+            </div>);
+        }else {
+            return(<div
+                className={containerClass}
+                style={{marginBottom: outerMarginBottom + `rem`}}
+            >
+                <RichText.Content
+                    tagName={tagName}
+                    value={title}
+                    style={{
+                        color: titleColor,
+                        fontSize: titleSize + 'rem',
+                        textAlign: align,
+                        marginBottom: titleMarginBottom + 'rem'
+                    }}
+                    className={`vk_heading_title vk_heading_title-style-${titleStyle}`}
+                />
+                {
+                    // サブテキスト
+                    (() => {
+                        if (subTextFlag === 'on') {
+                            return (
+                                <RichText.Content
+                                    tagName={'p'}
+                                    value={subText}
+                                    style={{
+                                        color: subTextColor,
+                                        fontSize: subTextSize + 'rem',
+                                        textAlign: align
+                                    }}
+                                    className={`vk_heading_subtext vk_heading_subtext-style-${titleStyle}`}
+                                />
+                            );
+                        }
+                    })()
+                }
+            </div>);
+        }
     },
 
     deprecated: Version0_6_0
