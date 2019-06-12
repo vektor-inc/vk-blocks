89997d0ac53da85d5728618c1406cf16e0235127
kurudrive <kurudrive@gmail.com>
2019-06-12 09:21:29 +0900

    [ toc ] 自動番号付与

--- src/table-of-contents/TableOfContents.js
+++ src/table-of-contents/TableOfContents.js
@@ -61,6 +61,20 @@ class TableOfContents {
         }
 
         let listClassName = 'vk_tableOfContents_list_item';
+        let countSeparater = '.';
+        let h2Count = 0;
+        let h3Count = 0;
+        let h4Count = 0;
+        let h5Count = 0;
+        let h6Count = 0;
+        const fixZero = (count) => {
+          if ( count == 0 ){
+            return 1;
+          } else {
+            return count;
+          }
+        };
+
 
         let returnHtml = <div className={className + 'vk_tableOfContents' + style}>
             <div className={'vk_tableOfContents_title'}>{__('Table of Contents', 'vk-blocks')}</div>
@@ -70,9 +84,59 @@ class TableOfContents {
                     let baseClass = 'vk_tableOfContents_list_item';
 
                     let level = data.tagName.replace( /H/g , '' ) ;
+
+                    let preNumber = '';
+
+                    if ( level == 2 ){
+                      h2Count++;
+                      preNumber = h2Count;
+
+                      // Reset
+                      h3Count = 0;
+                      h4Count = 0;
+                      h5Count = 0;
+                      h6Count = 0;
+                    }
+
+                    if ( level == 3 ){
+                      h3Count++;
+                      preNumber = h2Count + countSeparater + h3Count;
+
+                      // Reset
+                      h4Count = 0;
+                      h5Count = 0;
+                      h6Count = 0;
+                    }
+
+                    if ( level == 4 ){
+                      h4Count++;
+                      preNumber = h2Count + countSeparater + fixZero(h3Count) + countSeparater + h4Count;
+
+                      // Reset
+                      h5Count = 0;
+                      h6Count = 0;
+                    }
+
+                    if ( level == 5 ){
+                      h5Count++;
+                      preNumber = h2Count + countSeparater + fixZero(h3Count) + countSeparater + fixZero(h4Count) + countSeparater + h5Count;
+
+                      // Reset
+                      h6Count = 0;
+                    }
+
+                    if ( level == 6 ){
+                      h6Count++;
+                      preNumber = h2Count + countSeparater + fixZero(h3Count) + countSeparater + fixZero(h4Count) + countSeparater + fixZero(h5Count) + countSeparater + h6Count;
+
+                    }
+
+                    preNumber = preNumber + '. ';
+
                     return <li className={`${baseClass} ${baseClass}-h-${level}`}>
                             <a href="" className={`${baseClass}_link`}>
-                                {data.innerText}
+                                <span className={`${baseClass}_link_preNumber`}>{preNumber}</span>
+																{data.innerText}
                             </a>
                         </li>;
                 })}
