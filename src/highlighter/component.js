3ff295e19572437487abd3be446279e750213298
naoki <n.globe.us@gmail.com>
2019-07-18 14:23:41 +0900

    add template

---
+++ src/highlighter/component.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class NewComponent extends React.Component {
+
+    render() {
+
+        let {
+            heading,
+            content,
+            insertImage,
+            arrowFlag,
+        } = this.props.attributes;
+        let className = this.props.className;
+        let for_ = this.props.for_;
+
+        return (
+            <div className={"vk_highlighter"}>hello</div>
+        );
+    }
+}
