fadf0b1e8c526dff66a45cd56b09a679847c2aa1
naoki <n.globe.us@gmail.com>
2019-07-18 17:03:20 +0900

    [ add/highlighter ] add span + class to selected text

--- src/highlighter/block.js
+++ src/highlighter/block.js
@@ -2,95 +2,79 @@
  * highlighter block type
  *
  */
-import NewComponent from "./component";
-import {schema} from './schema';
 
 const {__} = wp.i18n; // Import __() from wp.i18n
-const {registerBlockType} = wp.blocks; // Import registerBlockType() from wp.blocks
+const {registerFormatType, toggleFormat, applyFormat, removeFormat, getActiveFormat} = window.wp.richText;
+const {RichTextToolbarButton, RichTextShortcut, InspectorControls, ColorPalette, PanelColorSettings, getColorObjectByColorValue} = wp.editor;
 const {RangeControl, RadioControl, PanelBody, Button, PanelColor, BaseControl} = wp.components;
 const {Fragment} = wp.element;
-const {RichText, InspectorControls, MediaUpload, ColorPalette} = wp.editor;
+const {
+    select
+} = wp.data;
+const name = 'vk-blocks/highlighter';
 const BlockIcon = 'arrow-down';
 
-/**
- * Register: a Gutenberg Block.
- *
- * Registers a new block provided a unique name and an object defining its
- * behavior. Once registered, the block is made editor as an option to any
- * editor interface where blocks are implemented.
- *
- * @link https://wordpress.org/gutenberg/handbook/block-api/
- * @param  {string}   name     Block name.
- * @param  {Object}   settings Block settings.
- * @return {?WPBlock}          The block, if it has been successfully
- *                             registered; otherwise `undefined`.
- */
-registerBlockType('vk-blocks/highlighter', {
-    // Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-    title: __('highlighter', 'vk-blocks'), // Block title.
-    icon: BlockIcon, // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
-    category: 'vk-blocks-cat', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
-    attributes: schema,
 
-    /**
-     * The edit function describes the structure of your block in the context of the editor.
-     * This represents what the editor will render when the block is used.
-     *
-     * The "edit" property must be a valid function.
-     *
-     * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-     */
-    edit({attributes, setAttributes,className}) {
-        const {
-            heading,
-            content,
-            insertImage,
-            arrowFlag,
-        } = attributes;
+registerFormatType(name, {
+    title: __('Highlighter', 'vk-blocks'),
+    tagName: 'span',
+    className: 'vk_highlighter',
+    attributes: {
+        data: 'data-color',
+        class: 'class'
+    },
+    edit(props) {
+        const {value, isActive, onChange} = props;
+        const onToggle = () => onChange(
+            toggleFormat(value, {type: name})
+        );
+
+        // let activeColor;
+        // // 設定したカラーパレーットを読み込む
+        // const colorSet = select('core/editor').getEditorSettings().colors;
+        //
+        // // 使用しているカラーを選択する
+        // if (isActive) {
+        //     const activeFormat = getActiveFormat(value, type);
+        //     activeColor = activeFormat.attributes.data;
+        // }
 
         return (
             <Fragment>
-                <InspectorControls>
-                    <PanelBody title={__('Color Setting', 'vk-blocks')}>
-                        <BaseControl label={__('Title Color', 'vk-blocks')}>
-                            <ColorPalette
-                                value={content}
-                                onChange={(value) => setAttributes({content: value})}
-                            />
-                        </BaseControl>
-                    </PanelBody>
-                </InspectorControls>
-                <div className="vk_spacer">
-                    <div>Editor</div>
-                    <NewComponent
-                        attributes={attributes}
-                        className={className}
-                        for_={'edit'}
-                    />
-                </div>
-            </Fragment>
-        );
-    },
-
-    /**
-     * The save function define className }> which the different attributes should be combined
-     * into the final markup, which is then serialized by Gutenberg into post_content.
-     *
-     * The "save" property must be specified and must be a valid function.
-     *
-     * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-     */
-    save({attributes,className}) {
-
-        return (
-            <div className="vk_highlighter">
-                <div>Front</div>
-                <NewComponent
-                    attributes={attributes}
-                    className={className}
-                    for_={'save'}
+                {/*<InspectorControls>*/}
+                {/*    <PanelColorSettings*/}
+                {/*        title={__('Highlighter', 'vk-blocks')}*/}
+                {/*        initialOpen={false}*/}
+                {/*        colorSettings={[*/}
+                {/*            {*/}
+                {/*                value: activeColor,*/}
+                {/*                onChange: (color) => {*/}
+                {/*                    if (color) {*/}
+                {/*                        // 選択しているカラーを取得*/}
+                {/*                        const colorObject = getColorObjectByColorValue(colorSet, color);*/}
+                {/*                        onChange(applyFormat(value, {*/}
+                {/*                            type: {name},*/}
+                {/*                            attributes: {*/}
+                {/*                                data: color,*/}
+                {/*                                class: `has-${colorObject.slug}`*/}
+                {/*                            }*/}
+                {/*                        }));*/}
+                {/*                        return*/}
+                {/*                    }*/}
+                {/*                    onChange(removeFormat(value, type))*/}
+                {/*                },*/}
+                {/*                label: '選択色'*/}
+                {/*            }*/}
+                {/*        ]}*/}
+                {/*    />*/}
+                {/*</InspectorControls>*/}
+                <RichTextToolbarButton
+                    icon={BlockIcon}
+                    title={__('Highlighter', 'vk-blocks')}
+                    onClick={onToggle}
+                    isActive={isActive}
                 />
-            </div>
+            </Fragment>
         );
     },
 });
