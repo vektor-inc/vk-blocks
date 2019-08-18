7cd4c5c3a973d3479880db69b7b97312e9f0fadc
kurudrive <kurudrive@gmail.com>
2019-08-19 01:09:21 +0900

    [ font awesome ] Change to 5.10.1

--- inc/font-awesome/font-awesome-config.php
+++ inc/font-awesome/font-awesome-config.php
@@ -3,10 +3,10 @@
 /*  Load modules
 /*-------------------------------------------*/
 if ( ! class_exists( 'Vk_Font_Awesome_Versions' ) ) {
-	require_once( 'font-awesome/class-vk-font-awesome-versions.php' );
+	require_once( 'package/class-vk-font-awesome-versions.php' );
 
 	global $font_awesome_directory_uri;
-	$font_awesome_directory_uri = plugins_url( '', __FILE__ ) . '/font-awesome/';
+	$font_awesome_directory_uri = plugins_url( '', __FILE__ ) . '/package/';
 
 	global $set_enqueue_handle_style;
 	$set_enqueue_handle_style = 'vkExUnit_common_style';
