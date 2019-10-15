c8c36c58d73c5f20c3610376e1cc4fd0cab005fc
kurudrive <kurudrive@gmail.com>
2019-10-15 18:29:31 +0900

    [ Change version ] 0.11.0

--- inc/vk-blocks-config.php
+++ inc/vk-blocks-config.php
@@ -12,7 +12,7 @@ if ( ! function_exists( 'vkblocks_active' ) ) {
 	// Set asset URL.
 	define( 'VK_BLOCKS_URL', plugin_dir_url( __FILE__ ) . 'vk-blocks/' );
 	// Set version number.
-	define( 'VK_BLOCKS_VERSION', '0.10.1' );
+	define( 'VK_BLOCKS_VERSION', '0.11.0' );
 
 
 	global $vk_blocks_prefix;
