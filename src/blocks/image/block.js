7465e32f8b6a65dba4f8bbf70d5e90e7e4011554
bitbucket-piplines-vk-blocks-pro <info@vektor-inc.co.jp>
2020-10-07 06:12:24 +0000

    Update from vk-blocks-pro

--- src/blocks/image/block.js
+++ src/blocks/image/block.js
@@ -9,5 +9,34 @@ wp.blocks.registerBlockStyle('core/image',
 			name: 'vk-image-photoFrame',
 			label: __('Photo frame', 'vk-blocks'),
 		},
+		{
+			name: 'vk-image-photoFrame-tilt-right',
+			label: __('Photo frame Tilt Right', 'vk-blocks'),
+		},
+		{
+			name: 'vk-image-photoFrame-tilt-left',
+			label: __('Photo frame Tilt Left', 'vk-blocks'),
+		},
+		{
+			name: 'vk-image-shadow',
+			label: __('Shadow', 'vk-blocks'),
+		},
+		{
+			name: 'vk-image-wave01',
+			label: __('Wave01', 'vk-blocks'),
+		},
+		{
+			name: 'vk-image-wave02',
+			label: __('Wave02', 'vk-blocks'),
+		},
+		{
+			name: 'vk-image-wave03',
+			label: __('Wave03', 'vk-blocks'),
+		},
+		{
+			name: 'vk-image-wave04',
+			label: __('Wave04', 'vk-blocks'),
+		},
+			
 	]
 );
