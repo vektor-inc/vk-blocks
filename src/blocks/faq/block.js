54b199cda4614138fdd3511d31d0b3a23303de1d
bitbucket-piplines-vk-blocks-pro <info@vektor-inc.co.jp>
2020-10-09 08:02:41 +0000

    Update from vk-blocks-pro

--- src/blocks/faq/block.js
+++ src/blocks/faq/block.js
@@ -8,8 +8,10 @@ import classNames from "classnames";
 import { content, title } from "./../_helper/example-data"
 
 const { __ } = wp.i18n;
+const { Fragment } = wp.element;
 const { registerBlockType } = wp.blocks;
-const { RichText, InnerBlocks } = vkbBlockEditor;
+const { PanelBody, PanelRow } = wp.components;
+const { RichText, InnerBlocks, InspectorControls } = vkbBlockEditor;
 
 const BlockIcon = (
 	<svg
@@ -104,38 +106,53 @@ registerBlockType("vk-blocks/faq", {
 		],
 	},
 
-  edit({ attributes, setAttributes, className }) {
-	const { heading, content } = attributes;
-	const TEMPLATE = [
-		[ 'core/paragraph', { content} ],
-	];
+  	edit({ attributes, setAttributes, className }) {
+		let massage;
+		if ( vk_blocks_check.is_pro ) {
+			massage = __( 'If you want to be collapsing this block, you can set it at Setting > VK Blocks', 'vk-blocks' );
+		} else {
+			massage = __( 'You can be collapsing this block at VK Blocks Pro', 'vk-blocks' );
+		}
+		const { heading, content } = attributes;
+		const TEMPLATE = [
+			[ 'core/paragraph', { content} ],
+		];
 
-    return (
-	<dl className={ classNames(className,"vk_faq") }>
-		<RichText
-			tagName="dt"
-			className={ "vk_faq_title" }
-			onChange={ value => setAttributes({ heading: value }) }
-			value={ heading }
-			placeholder={ __("Please enter a question.", "vk-blocks") }
-        />
-		<dd className={ `vk_faq_content` }> <InnerBlocks template={ TEMPLATE } /></dd>
-	</dl>
-	  );
+		return (
+			<Fragment>
+				<InspectorControls>
+					<PanelBody title={ __('Accordion Setting', 'vk-blocks') }>
+						<PanelRow>{ massage }</PanelRow>
+					</PanelBody>
+				</InspectorControls>
+				<dl className={ classNames(className,"vk_faq") }>
+					<RichText
+						tagName="dt"
+						className={ "vk_faq_title" }
+						onChange={ value => setAttributes({ heading: value }) }
+						value={ heading }
+						placeholder={ __("Please enter a question.", "vk-blocks") }
+					/>
+					<dd className={ `vk_faq_content` }> <InnerBlocks template={ TEMPLATE } /></dd>
+				</dl>
+			</Fragment>
+		);
 	},
 
 	save({ attributes }) {
-	const { heading} = attributes;
-	return (
-		<dl className={ `vk_faq` }>
-			<RichText.Content
-				tagName="dt"
-				className={ "vk_faq_title" }
-				value={ heading }
-        />
-			<dd className={ `vk_faq_content` }> <InnerBlocks.Content /></dd>
-		</dl>
-    );
-  },
-  deprecated
+		const { heading} = attributes;
+		return (
+			<dl className={ `vk_faq [accordion_trigger_switch]` }>
+				<RichText.Content
+					tagName="dt"
+					className={ "vk_faq_title" }
+					value={ heading }
+				/>
+				<dd className={ `vk_faq_content` }>
+					<InnerBlocks.Content />
+				</dd>
+			</dl>
+		);
+  	},
+  	deprecated
 });
