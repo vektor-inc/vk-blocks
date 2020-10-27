9b41bedb1a1fa29e76a440845a897ba7515966ae
bitbucket-piplines-vk-blocks-pro <info@vektor-inc.co.jp>
2020-10-27 02:55:18 +0000

    Update from vk-blocks-pro

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,45 @@ const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
 const cleanCss = require("gulp-clean-css");
 const replace = require('gulp-replace');
+const rename = require("gulp-rename");
 
 // js最小化
 const jsmin = require("gulp-jsmin");
 
-gulp.task('text-domain', function (done) {
-	gulp.src(['./inc/term-color/package/*'])
-	  .pipe(replace(', \'vk_term_color_textdomain\'', ', \'vk-blocks\''))
-    .pipe(gulp.dest('./inc/term-color/package/'));
-  gulp.src(['./inc/vk-components/package/*'])
-  .pipe(replace(', \'vk_components_textdomain\'', ', \'vk-blocks\''))
-  .pipe(gulp.dest('./inc/vk-components/package/'));
+gulp.task( 'helper-js', function (done)  {
+	gulp.src('src/blocks/helper/faq2.js')
+	.pipe(jsmin())
+	.pipe(rename({
+		extname: '.min.js'
+	}))
+	.pipe(gulp.dest('./inc/vk-blocks/build/'));
 	done();
-  });
+});
+
+gulp.task( 'helper-js-pro', function (done)  {
+	gulp.src('src/blocks/_pro/animation/enque-front.js')
+	.pipe(jsmin())
+	.pipe(rename('vk-animation.min.js'))
+	.pipe(gulp.dest('./inc/vk-blocks/build/'));
+	gulp.src('src/blocks/_pro/slider/enque-front.js')
+	.pipe(jsmin())
+	.pipe(rename('vk-slider.min.js'))
+	.pipe(gulp.dest('./inc/vk-blocks/build/'));
+	done();
+});
+
+// gulp.task('text-domain', function (done) {
+// 	gulp.src(['./inc/term-color/package/*'])
+// 	  .pipe(replace(', \'vk_term_color_textdomain\'', ', \'vk-blocks\''))
+//     .pipe(gulp.dest('./inc/term-color/package/'));
+//   gulp.src(['./inc/vk-components/package/*'])
+//     .pipe(replace(', \'vk_components_textdomain\'', ', \'vk-blocks\''))
+//     .pipe(gulp.dest('./inc/vk-components/package/'));
+//   gulp.src(['./inc/vk-css-optimize/package/*'])
+//     .pipe(replace(', \'css_optimize_textdomain\'', ', \'vk-blocks\''))
+//     .pipe(gulp.dest('./inc/vk-css-optimize/package/'));
+// 	done();
+//   });
 
 gulp.task("sass", function() {
   return (
@@ -52,6 +78,7 @@ gulp.task("sass", function() {
 gulp.task("sass_editor", function() {
   return gulp
     .src([
+      "./editor-css/_editor_common_core.scss",
       "./editor-css/_editor_before.scss",
       "./src/**/*.scss",
       "./editor-css/_editor_after.scss"
@@ -127,16 +154,18 @@ gulp.task("watch", function() {
 });
 
 //Build : Development
-gulp.task("build-dev", gulp.series("js-dev", "sass", "sass_editor","sass_bootstrap","sass_vk_components", "dist_swiper_js", "dist_swiper_css"));
+gulp.task("build:dev:free", gulp.series("js-dev", "sass", "helper-js", "sass_editor","sass_bootstrap","sass_vk_components", "dist_swiper_js", "dist_swiper_css"));
+gulp.task("build:dev:pro", gulp.series("js-dev", "sass", "helper-js", "helper-js-pro", "sass_editor","sass_bootstrap","sass_vk_components", "dist_swiper_js", "dist_swiper_css"));
 
 // Build : Production
-gulp.task("build", gulp.series("js", "sass", "sass_editor","sass_bootstrap","sass_vk_components", "dist_swiper_js", "dist_swiper_css"));
+gulp.task("build:free", gulp.series("js", "sass", "helper-js", "sass_editor","sass_bootstrap","sass_vk_components", "dist_swiper_js", "dist_swiper_css"));
+gulp.task("build:pro", gulp.series("js", "sass", "helper-js", "helper-js-pro", "sass_editor","sass_bootstrap","sass_vk_components", "dist_swiper_js", "dist_swiper_css"));
 
 // Default Tasks
 gulp.task("default", gulp.series("watch"));
 
 // replace_text_domain
-gulp.task("replace_text_domain", function(done) {
+gulp.task("text-domain", function(done) {
 	// font-awesome.
 	gulp.src(["./inc/font-awesome/package/*.php"])
 		.pipe(replace("'vk_font_awesome_version_textdomain'", "'vk-blocks'"))
@@ -152,7 +181,10 @@ gulp.task("replace_text_domain", function(done) {
 	// vk-components.
 	gulp.src(["./inc/vk-components/package/*.php"])
 		.pipe(replace("'vk_components_textdomain'","'vk-blocks'"))
-		.pipe(gulp.dest("./inc/vk-components/package/"));
+    .pipe(gulp.dest("./inc/vk-components/package/"));
+  gulp.src(["./inc/vk-css-optimize/package/*.php"])
+		.pipe(replace("'css_optimize_textdomain'","'vk-blocks'"))
+		.pipe(gulp.dest("./inc/vk-css-optimize/package/"));
 	done();
 });
 
--- inc/vk-css-optimize/vk-css-optimize-config.php
+++ inc/vk-css-optimize/vk-css-optimize-config.php
@@ -2,65 +2,51 @@
 /**
  * VK CSS Tree Shaking Config
  *
- * @package Lightning
+ * @package VK Blocks
  */
-
- /**
-  * Optimize CSS.
-  */
-function vk_blocks_optimize_css() {
-	$options = vkblocks_get_options();
-
-	if ( ! isset( $options['css_optimize'] ) ) {
-		$options['css_optimize'] = 'default';
-	} elseif ( 'optomize-all-css' === $options['css_optimize'] ) {
-		$options['css_optimize'] = 'tree-shaking';
-		update_option( 'vk_blocks_options', $options );
-	}
-
-	if ( ! empty( $options['css_optimize'] ) && ( 'optomize-all-css' === $options['css_optimize'] || 'tree-shaking' === $options['css_optimize'] ) ) {
-
-		// 表示位置の配列.
-		global $vk_css_tree_shaking_array;
-		if ( empty( $vk_css_tree_shaking_array ) ) {
-			$vk_css_tree_shaking_array = array(
-				array(
-					'id'      => 'vk-blocks-build-css',
-					'url'     => VK_BLOCKS_URL . 'build/block-build.css',
-					'path'    => VK_BLOCKS_PATH . 'build/block-build.css',
-					'version' => VK_BLOCKS_VERSION,
-				),
-			);
-		} else {
-			$add_array = array(
-				'id'      => 'vk-blocks-build-css',
-				'url'     => VK_BLOCKS_URL . 'build/block-build.css',
-				'path'    => VK_BLOCKS_PATH . 'build/block-build.css',
-				'version' => VK_BLOCKS_VERSION,
-			);
-			array_push( $vk_css_tree_shaking_array, $add_array );
+	
+function vkblocks_load_css_optimize(){
+	$theme_textdomain = wp_get_theme()->get( 'TextDomain' );
+	// VK Blocks は翻訳作るのが面倒なのでテーマ側が翻訳してある場合はそちらを使う
+	if ( 
+		'lightning' != $theme_textdomain && 
+		'lightning-pro' != $theme_textdomain && 
+		'katawara' != $theme_textdomain
+		){
+			if ( ! class_exists( 'VK_CSS_Optimize' ) ) {
+				require_once dirname( __FILE__ ) . '/package/class-vk-css-optimize.php';
+			}
 		}
-
-		$vk_css_tree_shaking_array = apply_filters( 'vk_css_tree_shaking_array', $vk_css_tree_shaking_array );
-		if ( ! class_exists( 'VK_CSS_Optimize' ) ) {
-			require_once dirname( __FILE__ ) . '/package/class-vk-css-optimize.php';
-		}
-	}
 }
-add_action( 'after_setup_theme', 'vk_blocks_optimize_css' );
+add_action( 'after_setup_theme', 'vkblocks_load_css_optimize',11 );
+
+function vkblocks_css_tree_shaking_array( $vk_css_tree_shaking_array ){
+	$vk_css_tree_shaking_array[] = array(
+		'id'      => 'vk-blocks-build-css',
+		'url'     => VK_BLOCKS_URL . 'build/block-build.css',
+		'path'    => VK_BLOCKS_PATH . 'build/block-build.css',
+		'version' => VK_BLOCKS_VERSION,
+	);
+	return $vk_css_tree_shaking_array;
+}
+add_filter( 'vk_css_tree_shaking_array', 'vkblocks_css_tree_shaking_array' );
 
 /**
  * CSS Tree Shaking Exclude
  *
  * @param array $inidata CSS Tree Shaking Exclude Paramator.
  */
-function vk_blocks_css_tree_shaking_exclude_class( $inidata ) {
+function vkblocks_css_tree_shaking_exclude_class( $inidata ) {
 	$exclude_classes_array = array(
 		'vk_animation-active',
 		'vk_borderBox_title',
+		'vk_faq-accordion-open',
+		'vk_faq-accordion-close',
+		'vk_faq_content-accordion-open',
+		'vk_faq_content-accordion-close',
 	);
 	$inidata['class']      = array_merge( $inidata['class'], $exclude_classes_array );
 
 	return $inidata;
 }
-add_filter( 'css_tree_shaking_exclude', 'vk_blocks_css_tree_shaking_exclude_class' );
+add_filter( 'css_tree_shaking_exclude', 'vkblocks_css_tree_shaking_exclude_class' );
