5ee4beb4e7b2bbd3eaec8145b7353b2c2d1156bc
bitbucket-piplines-vk-blocks-pro <info@vektor-inc.co.jp>
2020-11-04 06:03:34 +0000

    Update from vk-blocks-pro

---
+++ src/blocks/pr-content/deprecated/0.43.0/schema.js
@@ -0,0 +1,63 @@
+export default {
+	title: {
+		source: "html",
+		selector: ".vk_prContent_colTxt_title"
+	},
+	titleColor: {
+		type: "string",
+		default: ""
+	},
+	content: {
+		source: "html",
+		selector: ".vk_prContent_colTxt_text"
+	},
+	contentColor: {
+		type: "string",
+		default: ""
+	},
+	url: {
+		type: "string",
+		default: ""
+	},
+	buttonType: {
+		type: "string",
+		default: "0"
+	},
+	buttonColor: {
+		type: "string",
+		default: "primary"
+	},
+	buttonColorCustom: {
+		type: "string",
+		default: ""
+	},
+	buttonText: {
+		source: "html",
+		selector: ".vk_button_link_txt",
+		default: ""
+	},
+	buttonTarget: {
+		type: "Boolean",
+		default: false
+	},
+	Image: {
+		type: "string",
+		default: "{}"
+	},
+	ImageBorderColor: {
+		type: "string",
+		default: ""
+	},
+	layout: {
+		type: "string",
+		default: "left"
+	},
+	fontAwesomeIconBefore: {
+		type: "string",
+		default: '<i class="fas fa-user"></i>'
+	},
+	fontAwesomeIconAfter: {
+		type: "string",
+		default: '<i class="fas fa-user"></i>'
+	}
+};
