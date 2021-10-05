b61ba6e932abee7d8018ef939ec3d6a9afe6bd43
bitbucket-piplines-vk-blocks-pro <info@vektor-inc.co.jp>
2021-10-05 16:01:32 +0000

    Update from vk-blocks-pro

--- vk-blocks.php
+++ vk-blocks.php
@@ -3,7 +3,7 @@
  * Plugin Name: VK Blocks 
  * Plugin URI: https://github.com/vektor-inc/vk-blocks
  * Description: This is a plugin that extends Gutenberg's blocks.
- * Version: 1.16.1
+ * Version: 1.16.2
  * Requires at least: 5.7
  * Author: Vektor,Inc.
  * Author URI: https://vektor-inc.co.jp
@@ -104,6 +104,10 @@ require_once plugin_dir_path( __FILE__ ) . 'inc/vk-blocks-config.php';
  */
 $vk_blocks_plugin_base_dir = plugin_dir_path( __FILE__ );
 if ( strpos( $vk_blocks_plugin_base_dir, 'vk-blocks-pro' ) !== false ) {
+
+	// Cope with : WP HTTP Error: cURL error 60: SSL certificate problem: certificate has expired.
+	add_filter( 'https_ssl_verify', '__return_false' );
+
 	$vk_blocks_updater_url = plugin_dir_path( __FILE__ ) . 'inc/vk-blocks-pro/plugin-update-checker/plugin-update-checker.php';
 	if ( file_exists( $vk_blocks_updater_url ) ) {
 		require plugin_dir_path( __FILE__ ) . 'inc/vk-blocks-pro/plugin-update-checker/plugin-update-checker.php';
