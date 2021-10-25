c01399b4b0b249b3a0d55a61a6a112a655b40517
bitbucket-piplines-vk-blocks-pro <info@vektor-inc.co.jp>
2021-10-25 13:10:37 +0000

    Update from vk-blocks-pro

--- vk-blocks.php
+++ vk-blocks.php
@@ -3,7 +3,7 @@
  * Plugin Name: VK Blocks 
  * Plugin URI: https://github.com/vektor-inc/vk-blocks
  * Description: This is a plugin that extends Gutenberg's blocks.
- * Version: 1.16.8
+ * Version: 1.16.9
  * Requires at least: 5.7
  * Author: Vektor,Inc.
  * Author URI: https://vektor-inc.co.jp
