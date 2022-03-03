fec52f2e3189b5beaaad372c71a2e0e8ba2a7694
bitbucket-piplines-vk-blocks-pro <info@vektor-inc.co.jp>
2022-03-03 10:16:17 +0000

    Update from vk-blocks-pro

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link:
 Tags: Gutenberg,FAQ,alert
 Requires at least: 5.8
 Tested up to: 5.9.1
-Stable tag: 1.27.5
+Stable tag: 1.26.2
 Requires PHP: 7.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
--- vk-blocks.php
+++ vk-blocks.php
@@ -3,8 +3,8 @@
  * Plugin Name: VK Blocks 
  * Plugin URI: https://github.com/vektor-inc/vk-blocks
  * Description: This is a plugin that extends Gutenberg's blocks.
- * Version: 1.27.5
- * Stable tag: 1.27.5
+ * Version: 1.27.6
+ * Stable tag: 1.26.2
  * Requires at least: 5.8
  * Author: Vektor,Inc.
  * Author URI: https://vektor-inc.co.jp
