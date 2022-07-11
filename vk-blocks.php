4b391e11360156d33359aa3fbf6b3e4b80746543
kurudrive <kurudrive@gmail.com>
2022-07-11 23:51:49 +0900

    [ Change version ] 1.39.1.2 / .distignore を /src/ に変更

--- vk-blocks.php
+++ vk-blocks.php
@@ -3,7 +3,7 @@
  * Plugin Name: VK Blocks
  * Plugin URI: https://github.com/vektor-inc/vk-blocks
  * Description: This is a plugin that extends Gutenberg's blocks.
- * Version: 1.39.1.1
+ * Version: 1.39.1.2
  * Stable tag: 1.37.0.0
  * Requires at least: 5.8
  * Author: Vektor,Inc.
