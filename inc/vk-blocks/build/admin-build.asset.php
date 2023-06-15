8ce78193b4f53ac92fa5d3468f02adb0bf249b3f
drill-lancer <rickaddison7634@gmail.com>
2023-06-15 12:06:47 +0900

    fix: deploy

--- inc/vk-blocks/build/admin-build.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-block-library', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill', 'wp-primitives'), 'version' => '4ac426816b4d5f5f20f6');
--- inc/vk-blocks/build/block-build.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-rich-text', 'wp-server-side-render'), 'version' => 'ef9302eeb066880acb01');
