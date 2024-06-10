f91697f0d9651ba46242b841863fe5793f154161
drill-lancer <rickaddison7634@gmail.com>
2024-06-10 09:34:48 +0900

    fix: test

--- bin/install-theme.sh
+++ bin/install-theme.sh
@@ -5,9 +5,11 @@ set -ex
 # -o 実行結果をファイルへ出力
 # -s ファイル出力時の進捗状況を非表示にする(エラーも非表示)
 # curl -s $WP_THEME -o theme.zip
-curl -s https://downloads.wordpress.org/theme/lightning.zip -o theme.zip
+curl -sL https://downloads.wordpress.org/theme/lightning.zip -o theme.zip
 # -d 指定したディレクトリに展開
-mkdir ./temp/
+rm -rf temp/themes
+mkdir -p temp
+mkdir -p temp/themes
 unzip theme.zip -d ./temp/themes
 # 展開したのでもとのzipファイルを削除 -f はエラーメッセージを表示しない
 rm -f theme.zip
