7316e25ce738482a3b9c8d29d6055d8b63f94e23
kurudrive <kurudrive@gmail.com>
2025-02-25 16:46:14 +0900

    [ Change version ] 1.96.2.1

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link:
 Tags: Gutenberg,FAQ,alert
 Requires at least: 6.4
 Tested up to: 6.7
-Stable tag: 1.95.0.4
+Stable tag: 1.96.2.1
 Requires PHP: 7.4
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
--- vk-blocks.php
+++ vk-blocks.php
@@ -3,8 +3,8 @@
  * Plugin Name: VK Blocks
  * Plugin URI: https://github.com/vektor-inc/vk-blocks
  * Description: This is a plugin that extends Block Editor.
- * Version: 1.96.2.0
- * Stable tag: 1.95.0.4
+ * Version: 1.96.2.1
+ * Stable tag: 1.96.2.1
  * Requires at least: 6.4
  * Author: Vektor,Inc.
  * Author URI: https://vektor-inc.co.jp
