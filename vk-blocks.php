eb2cde498dc1ea0c3a3e141a46d462529b7a0ad0
kurudrive <kurudrive@gmail.com>
2025-07-22 15:14:08 +0900

    [ Change version ] 1.108.0.1

--- vk-blocks.php
+++ vk-blocks.php
@@ -3,8 +3,8 @@
  * Plugin Name: VK Blocks
  * Plugin URI: https://github.com/vektor-inc/vk-blocks
  * Description: This is a plugin that extends Block Editor.
- * Version: 1.108.0.0
- * Stable tag: 1.107.0.2
+ * Version: 1.108.0.1
+ * Stable tag: 1.108.0.1
  * Requires at least: 6.5
  * Author: Vektor,Inc.
  * Author URI: https://vektor-inc.co.jp
